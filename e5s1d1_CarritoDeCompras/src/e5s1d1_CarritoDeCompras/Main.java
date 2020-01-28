73dce0fa47f7eef5e14dc47280ae48e25981577d
w17714mR <william.rehe@gmail.com>
2020-01-28 11:58:59 -0300

    CARRITO DE COMPRAS

---
+++ e5s1d1_CarritoDeCompras/src/e5s1d1_CarritoDeCompras/Main.java
@@ -0,0 +1,10 @@
+package e5s1d1_CarritoDeCompras;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ e5s1d1_CarritoDeCompras/src/modelo/Articulo.java
@@ -0,0 +1,11 @@
+package modelo;
+
+public class Articulo {
+
+	private int precio;
+	private String nombre;
+	private int codigo;
+	private int talla;
+	private String marca;
+	
+}
